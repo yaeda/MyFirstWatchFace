4e00be5584871ca4a7c2786d0d26e09af6d7095d
Takeshi YAEDA <t.yaeda@gmail.com>
2015-01-05 23:36:36 +0900

    Initial Commit

---
+++ wear/src/main/java/com/kazeor/myfirstwatchface/MyWatchFace.java
@@ -0,0 +1,57 @@
+package com.kazeor.myfirstwatchface;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.support.wearable.view.WatchViewStub;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class MyWatchFace extends Activity {
+
+    private TextView mTime, mBattery;
+
+    private final static IntentFilter INTENT_FILTER;
+    static {
+        INTENT_FILTER = new IntentFilter();
+        INTENT_FILTER.addAction(Intent.ACTION_TIME_TICK);
+        INTENT_FILTER.addAction(Intent.ACTION_TIMEZONE_CHANGED);
+        INTENT_FILTER.addAction(Intent.ACTION_TIME_CHANGED);
+    }
+
+    private final String TIME_FORMAT_DISPLAYED = "kk:mm a";
+
+    private BroadcastReceiver mTimeInfoReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            mTime.setText(new SimpleDateFormat(TIME_FORMAT_DISPLAYED).format(Calendar.getInstance().getTime()));
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_watch_face);
+        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+            @Override
+            public void onLayoutInflated(WatchViewStub stub) {
+                mTime = (TextView) stub.findViewById(R.id.watch_time);
+                mBattery = (TextView) stub.findViewById(R.id.watch_battery);
+                mTimeInfoReceiver.onReceive(MyWatchFace.this, registerReceiver(null, INTENT_FILTER)); // set the current time
+                registerReceiver(mTimeInfoReceiver, INTENT_FILTER);
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(mTimeInfoReceiver);
+    }
+}
